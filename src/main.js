4234af18c014496d5042c009214f8b111bf386de
onedayl <onedayl@Daisy>
2019-01-30 20:05:04 +0800

    fix vue-worker

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
-import VueWorker from 'vue-worker';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
-Vue.use(VueWorker);
 
 new Vue({
   render: h => h(App),
